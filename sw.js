2dd07793c2d9d9a793ac9df9e7fdd81a86569555
La-ardiente-DiablaXX <abrahammedinasevillano@gmail.com>
2026-08-19 00:05:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,43 @@
-const CACHE_NAME = "diabla-x-app-v1";
+const CACHE_NAME = "diabla-x-app-v2";
+
+const ARCHIVOS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
 
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ARCHIVOS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copia = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copia);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
